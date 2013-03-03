ed86a349182f1312ed4f35dde87203974c590d09
Robin Lambertz <robinpub@googlemail.com>
2013-03-03 12:30:46 +0000

    New hub server IP

---
+++ src/main/java/net/craftminecraft/bukkit/cmcplugin/CMCPlugin.java
@@ -0,0 +1,28 @@
+package net.craftminecraft.bukkit.cmcplugin;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CMCPlugin extends JavaPlugin implements Listener {
+	@Override
+	public void onEnable() {
+		this.getServer().getPluginManager().registerEvents(this, this);
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+	
+	@EventHandler
+	public void onPlayerConnect(PlayerLoginEvent ev) {
+		String address = ev.getAddress().toString().split("/")[1];
+		if (!(address.equals("199.101.51.252"))) {
+			this.getLogger().info(address);
+			ev.setResult(PlayerLoginEvent.Result.KICK_OTHER);
+			ev.setKickMessage("Please connect to playcmc.com or this server's domain !");
+		}
+	}
+}
