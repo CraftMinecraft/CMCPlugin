d1a82ea82e4bb6c6dc2e37a534abe1e891aa8cb3
FireBall1725 <treed@sunnking.com>
2013-03-05 22:18:34 -0500

    Updated Config Library

--- src/main/java/net/craftminecraft/bukkit/cmcplugin/config/MainConfig.java
+++ src/main/java/net/craftminecraft/bukkit/cmcplugin/config/MainConfig.java
@@ -1,11 +1,16 @@
 package net.craftminecraft.bukkit.cmcplugin.config;
 
+import java.io.File;
+
 import org.bukkit.plugin.Plugin;
 
 public class MainConfig extends Config{
 	public MainConfig(Plugin plugin) {
-		this.setFile(plugin);
+		CONFIG_FILE = new File(plugin.getDataFolder(), "config.yml");
+        CONFIG_HEADER = "PlotMe Configration File - See http://www.craftminecraft.net for settings";
 	}
 
 	public String ServerAddress = "your server address here";
+	
+	public String ApiKey = "";
 }
